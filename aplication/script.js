682a1249f8088a22dedd1f2fdaae5fb95b1a7cad
Miroslav Jovic <jovicmiroslav88@gmail.com>
2022-06-16 18:21:10 +0200

    initial commit

---
+++ aplication/script.js
@@ -0,0 +1,31 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minsElement = document.getElementById('mins');
+const secondsElement = document.getElementById('seconds');
+
+const newYear = '1 Jan 2023';
+
+function countdownToNY() {
+    const dateOfNewYear = new Date(newYear);
+    const currentDate = new Date();
+
+    const allSeconds = (dateOfNewYear - currentDate) / 1000;
+
+    const days = Math.floor(allSeconds / 60 / 60 / 24);
+    const hours = Math.floor(allSeconds / 60 / 60) % 24;
+    const mins = Math.floor(allSeconds / 60) % 60;
+    const seconds = Math.floor(allSeconds) % 60;
+
+    daysElement.innerHTML = formatTime(days);
+    hoursElement.innerHTML = formatTime(hours);
+    minsElement.innerHTML = formatTime(mins);
+    secondsElement.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time
+}
+
+// initial call
+countdownToNY();
+setInterval(countdownToNY, 1000)
